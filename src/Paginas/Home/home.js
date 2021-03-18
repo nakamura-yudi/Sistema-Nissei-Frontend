34e456fda17b26a9c02d35f15c0f61c368f83c9e
Mariana Aya <umemuramariana@gmail.com>
2021-03-18 13:27:05 -0300

    home css

--- src/Paginas/Home/home.js
+++ src/Paginas/Home/home.js
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from 'react';
 import api from '../../servicos/api';
 import history from '../../history'
+import "./home.css"
+
 function Home()
 {
     function logout(){
@@ -29,13 +31,15 @@ function Home()
     }
 
     return (
-    <div>
-        <button type="button" onClick={listarClientes}>Listar Clientes</button>
-        <button type="button" onClick={listarFuncionarios}>Listar Funcionarios</button>
-        <button type="button" onClick={cadastrarPeca}>CadastrarPeca</button>
-        <button type="button" onClick={cadastrarFuncionario}>Cadastrar Funcionário</button>
-        <button type="button" onClick={cadastrarCliente}>Cadastrar Clientes</button>
-        <button type="button" onClick={logout}>Sair</button>
+    <div class="background">
+        <div class="list">
+            <button type="button" onClick={listarClientes} class="button">Listar Clientes</button>
+            <button type="button" onClick={listarFuncionarios} class="button">Listar Funcionarios</button>
+            <button type="button" onClick={cadastrarPeca} class="button">Cadastrar Peça</button>
+            <button type="button" onClick={cadastrarFuncionario} class="button">Cadastrar Funcionário</button>
+            <button type="button" onClick={cadastrarCliente} class="button">Cadastrar Clientes</button>
+            <button type="button" onClick={logout} class="button">Sair</button>
+        </div>
     </div>
     );
 }

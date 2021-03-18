8a2ad90df1dce52cfcb21a84056a2f3d3c1ae512
Mariana Aya <umemuramariana@gmail.com>
2021-03-18 16:54:35 -0300

    info cliente css

--- src/Paginas/ClienteInfo/infoCliente.js
+++ src/Paginas/ClienteInfo/infoCliente.js
@@ -1,6 +1,9 @@
 import React, { useEffect, useState } from 'react';
 import api from '../../servicos/api';
 import history from '../../history'
+import '../../app.css'
+import './infoCliente.css'
+
 function Home()
 {
     const [codigo,setCodigo]=useState('');
@@ -42,40 +45,42 @@ function Home()
         history.goBack();
     }
     return (
-    <div>
-        <div>
-            <p>Nome:{nome}</p>
-            <p>Emal:{email}</p>
-            <p>CPF:{cpf}</p>
-        </div>
-        <div id="divTable">
-            <table id="tabela">
-                <thead>
-                    <tr>
-                        <td>Placa</td>
-                        <td>Desc</td>
-                        <td>Ano</td>
-                        <td>Ação</td>
-                    </tr>
-                </thead>
-                <tbody>
-                    {carros.map(carro=>(
-                        <tr key={carro.car_id}>
-                            <td>{carro.car_placa}</td>
-                            <td>{carro.car_modelo}</td>     
-                            <td>{carro.car_ano}</td>
-                            <td><button type="button" onClick={()=>editarCarro(carro.car_id)}>Editar</button></td>          
+    <div class="background">
+        <div class="card">
+            <div>
+                <p>Nome:{nome}</p>
+                <p>Emal:{email}</p>
+                <p>CPF:{cpf}</p>
+            </div>
+            <div id="divTable" class="table-carro">
+                <table id="tabela">
+                    <thead>
+                        <tr>
+                            <td>Placa</td>
+                            <td>Desc</td>
+                            <td>Ano</td>
+                            <td>Ação</td>
                         </tr>
-                    ))}
-                </tbody>
-            </table>
-        </div>
+                    </thead>
+                    <tbody>
+                        {carros.map(carro=>(
+                            <tr key={carro.car_id}>
+                                <td>{carro.car_placa}</td>
+                                <td>{carro.car_modelo}</td>     
+                                <td>{carro.car_ano}</td>
+                                <td><button type="button" onClick={()=>editarCarro(carro.car_id)} class="table-edit-carro">Editar</button></td>          
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
+            </div>
 
-        <div>
-        <button type="button" onClick={editarCliente}>Editar Cliente</button>
-            <button type="button" onClick={cadastrarCarro}>Cadastrar Carros</button>
-            <button type="button" onClick={cadastrarContato}>Cadastrar Contatos</button>
-            <button type="button" onClick={voltar}>Voltar</button>
+            <div>
+            <button type="button" onClick={editarCliente} class="button-info-cliente">Editar Cliente</button>
+                <button type="button" onClick={cadastrarCarro} class="button-info-cliente">Cadastrar Carros</button>
+                <button type="button" onClick={cadastrarContato} class="button-info-cliente">Cadastrar Contatos</button>
+                <button type="button" onClick={voltar} class="button-info-cliente">Voltar</button>
+            </div>
         </div>
     </div>
     );

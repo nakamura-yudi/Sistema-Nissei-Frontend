bc2b84b4224ac350dcd352ba80d4f53cccf68342
Mariana Aya <umemuramariana@gmail.com>
2021-03-18 13:02:37 -0300

    login css

--- src/Paginas/Login/login.js
+++ src/Paginas/Login/login.js
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
 import history from '../../history'
 import api from '../../servicos/api'
+import "./login.css"
+
 function Login(){
     const [user,setUser] = useState('');
     const [senha,setSenha]=useState('');
@@ -32,18 +34,18 @@ function Login(){
         history.push('/cadastroFuncionario');
     }
     return (
-      <div id='login'>
-          <main>
+      <div id='login' class="background">
+          <main class="main">
             <h2>LOGIN</h2>
-            <form onSubmit={logar}>
+            <form onSubmit={logar} class="form">
               <input type="text" data-user placeholder="Enter Email" id="user" name="user" value={user} onChange={e=>setUser(e.target.value)} required/>
               <input type="password" data-password placeholder="Enter Password" id="password" name="password" value={senha} onChange={e=>setSenha(e.target.value)} required/>
               <div id="mensagem">
 
               </div>
-              <button type="submit">Entrar</button>
+              <button type="submit" class="button">Entrar</button>
             </form>
-            <p onClick={irFormulario}>Não tem um cadastro? Cadraste-se aqui</p>
+            <p onClick={irFormulario} class="text-pointer">Não tem um cadastro? Cadraste-se aqui</p>
           </main>
       </div>
     );

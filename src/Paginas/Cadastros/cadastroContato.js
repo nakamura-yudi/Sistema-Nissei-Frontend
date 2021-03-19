c880575660e74c5db00364431dc1ac66ebd1359a
Bianca <umemurabianca@gmail.com>
2021-03-18 21:17:25 -0300

    cadastro contato

--- src/Paginas/Cadastros/cadastroContato.js
+++ src/Paginas/Cadastros/cadastroContato.js
@@ -51,85 +51,82 @@ function CadastroContatos(){
         e.preventDefault();
         let mensagem = document.querySelector("#mensagem");
         mensagem.innerHTML="";
-        if(tipo.length>0 && numero.length>0)
-        {
-            if(validarTelefone(numero) && !vazio(tipo)){
-               
-                const response=await api.post('/contatos',{
-                    pes_cod: cod,
-                    cont_numero: numero,
-                    cont_tipo: tipo
 
-                })
-                carregarContatos();
-                alert('Contato cadastrado');
-                setNumero('');
-                setTipo('');
-            }
-            else{
-                if(!validarTelefone(numero))
-                    mensagem.innerHTML="<p>Telefone inválido</p>";
-                if(vazio(tipo))
-                    mensagem.innerHTML="<p>Escolha o tipo</p>";
-                console.log('entrei para erro');
-            }
+        if(validarTelefone(numero) && !vazio(tipo)){
+            
+            const response=await api.post('/contatos',{
+                pes_cod: cod,
+                cont_numero: numero,
+                cont_tipo: tipo
+
+            })
+            carregarContatos();
+            alert('Contato cadastrado');
+            setNumero('');
+            setTipo('');
         }
+        else{
+            if(!validarTelefone(numero))
+                mensagem.innerHTML+="<p>Telefone inválido</p>";
+            if(vazio(tipo))
+                mensagem.innerHTML+="<p>Escolha o tipo</p>";
+        }
+        
     }
 
     return(
         <div id="tela" className="background">    
-            <div id="app">
-                <aside className="formularioCont" >
-                    <h1>Cadastrar Contato</h1>
-                    <form onSubmit={cadastrarContato} >
+            <aside className="formularioCont" >
+                <h1>Cadastrar Contato</h1>
+                <form onSubmit={cadastrarContato} >
 
-                        <div className="input-block" id="block-numeroContato">
-                            <label htmlFor="numeroContato">Numero</label>
-                            <input name="numeroContato" id="numeroContato" value={numero} onChange={e=>setNumero(e.target.value)} placeholder="(xx) xxxxx-xxxx" required/>
-                        </div>
-
-                        <div className="input-block" id="block-tpContato">
-                            <label htmlFor="tipoCont">Tipo de contato</label>
-                            <select id="tipoCont" onChange={e=>setTipo(e.target.value)} value={tipo}>
-                                <option id="op-vazio" valie=''>Selecione uma opção </option>
-                                <option id="op-contA" value="Residencial">Residencial</option>
-                                <option id="op-contB" value="Comercial">Comercial</option>
-                                <option id="op-contC" value="Celular">Celular</option>
-                            </select>
-                        </div>
-                 
-                        <div id="mensagem">
-
-                        </div>
-                        <button type="submit" id="btnForm">Cadastrar</button>
-                        <button type="button" className="btnVoltarPerf" onClick={voltarPerfil}>Voltar ao perfil</button>
-                    </form>
-                    <div id="divTable">
-                        <table id="tabelaCont">
-                            <thead>
-                                <tr>
-                                    <td>Tipo</td>
-                                    <td>Numero</td>
-                                    <td>Excluir</td>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                {contatos.map(contato=>(
-                                    <tr key={contato.cont_cod}>
-                                        <td>{contato.cont_tipo}</td>
-                                        <td>{contato.cont_numero}</td>
-                                        <td>
-                                            <button id="btexcluir" className="btnExcluirCont" onClick={()=>Excluir(contato.cont_cod)} type="button">
-                                                Excluir
-                                            </button>
-                                        </td>
-                                    </tr>
-                                ))}
-                            </tbody>
-                        </table>
+                    <div className="input-block" id="block-numeroContato">
+                        <label htmlFor="numeroContato">Numero</label>
+                        <input name="numeroContato" id="numeroContato" value={numero} onChange={e=>setNumero(e.target.value)} placeholder="(xx) xxxxx-xxxx" required/>
                     </div>
-                </aside>
-            </div>
+
+                    <div className="input-block" id="block-tpContato">
+                        <label htmlFor="tipoCont">Tipo de contato</label>
+                        <select id="tipoCont" onChange={e=>setTipo(e.target.value)} value={tipo}>
+                            <option id="op-vazio" valie=''>Selecione uma opção </option>
+                            <option id="op-contA" value="Residencial">Residencial</option>
+                            <option id="op-contB" value="Comercial">Comercial</option>
+                            <option id="op-contC" value="Celular">Celular</option>
+                        </select>
+                    </div>
+                
+                    <div id="mensagem">
+
+                    </div>
+                    <button type="submit" id="btnForm">Cadastrar</button>
+                    <button type="button" className="btnVoltarPerf" onClick={voltarPerfil}>Voltar ao perfil</button>
+                </form>
+                <div id="divTable">
+                    <table id="tabelaCont">
+                        <thead>
+                            <tr>
+                                <td>Tipo</td>
+                                <td>Numero</td>
+                                <td>Excluir</td>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {contatos.map(contato=>(
+                                <tr key={contato.cont_cod}>
+                                    <td>{contato.cont_tipo}</td>
+                                    <td>{contato.cont_numero}</td>
+                                    <td>
+                                        <button id="btexcluir" className="btnExcluirCont" onClick={()=>Excluir(contato.cont_cod)} type="button">
+                                            Excluir
+                                        </button>
+                                    </td>
+                                </tr>
+                            ))}
+                        </tbody>
+                    </table>
+                </div>
+            </aside>
+            
         </div>
     );
 }
